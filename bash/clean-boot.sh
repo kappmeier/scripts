fd98269729468b544fa0fe10c886258b16573cf3
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2018-09-05 21:46:10 +0200

    improved call style

--- bash/clean-boot.sh
+++ bash/clean-boot.sh
@@ -5,9 +5,10 @@
 echo Removing old kernels.
 
 echo Space on /boot: $(df --block-size=1M /boot | tail -1 | awk '{print $4}') MiB
-echo You are running on kernel $(uname -r)
+KERNEL=$(uname -r)
+echo You are running on kernel $KERNEL
 
-KERNEL_LIST=$(dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p')
+KERNEL_LIST=$(dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'$KERNEL'/q;p')
 
 if [ -z "$KERNEL_LIST" ];
 then
@@ -20,7 +21,7 @@ else
     if [[ $REPLY =~ ^[YyJj]$ ]]
     then
         echo Removing kernels.
-        $(dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p'| xargs sudo apt-get -y purge)
+        $(dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'$KERNEL'/q;p'| xargs sudo apt-get -y purge)
         echo Free space on /boot: $(df --block-size=1M /boot | tail -1 | awk '{print $4}') MiB
     else
         echo Not removing kernels.
