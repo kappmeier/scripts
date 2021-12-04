0572d3ce0c1407f651cf56554177bb5f8b44c847
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2021-12-04 22:48:48 +0100

    Fixed style

--- tests/test_arxify.py
+++ tests/test_arxify.py
@@ -2,12 +2,13 @@ import unittest
 
 from arxify import convert_line_to_arxiv
 
+
 class TestArxify(unittest.TestCase):
 
     def test_line(self):
         result = convert_line_to_arxiv(r"\section{sec} % section 2")
         self.assertEqual(result, '\\section{sec} %')
 
+
 if __name__ == '__main__':
     unittest.main()
-
