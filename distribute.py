999225db60ac8eace7ac9c13b4a7e0dd4ac4731e
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2021-12-30 21:27:07 +0100

    Small script to distribute files based on patterns
    Files from a directory satisfying a regular expression are distributed
    into a target directory. Target (sub-)diretory and file name can be
    defined with respect to groups form the matched pattern.

---
+++ distribute.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+# Moves files from a folder to its destination
+#
+# Files are moved with respect to regular expressions.
+from logging import basicConfig, debug, info, DEBUG, INFO
+from os import path, listdir
+from pathlib import Path
+from re import compile
+from shutil import copyfile, move
+
+from tap import Tap
+
+_INDEX_PATTERN = r"\{([0-9]+)\}"
+_INDEX_REGEX = compile(_INDEX_PATTERN)
+
+
+class Distribution:
+    def __init__(self, pattern, target_directory, target_file_name):
+        self.target_directory = target_directory
+        self.target_file_name = target_file_name
+        self.regex = compile(pattern)
+        self.target_directory_indices = [int(x) for x in _INDEX_REGEX.findall(target_directory)]
+        debug("Target directory indices: %s", self.target_directory_indices)
+        self.target_file_name_indices = [int(x) for x in _INDEX_REGEX.findall(target_file_name)]
+        debug("Target file name indices: %s", self.target_file_name_indices)
+
+    def match(self, candidate_file):
+        match = self.regex.match(candidate_file)
+        return DistributionMatch(match, self) if match is not None else None
+
+
+class DistributionMatch:
+    def __init__(self, match, dist: Distribution):
+        self.match = match
+        self.distribution = dist
+        debug("groups: {}".format(match.groups()))
+        self.target_directory()
+
+    def target_directory(self):
+        target_directory_fill_values = [self.match.groups()[i] for i in
+                                        self.distribution.target_directory_indices]
+        target_directory_format = _INDEX_REGEX.sub('{}', self.distribution.target_directory)
+        return target_directory_format.format(*target_directory_fill_values)
+
+    def target_file(self):
+        target_file_name_fill_values = [self.match.groups()[i] for i in
+                                        self.distribution.target_file_name_indices]
+        target_file_format = _INDEX_REGEX.sub('{}', self.distribution.target_file_name)
+        return target_file_format.format(*target_file_name_fill_values)
+
+
+class CreateDistributeArgumentParser(Tap):
+    pattern: str
+    """Pattern used to match files."""
+    target_directory: str
+    """Target directory for distribution. Can contain matched groups as `{i}`."""
+    target_file_name: str
+    """File name for distribution. Can contain matched groups as `{i}`."""
+    directory: str  # Source/input directory
+    """The directory containing the source files."""
+    check: bool = False
+    """When enabled, files are not actually distributed."""
+    copy: bool = False
+    """When enabled, files are copied and not moved"""
+    verbose: bool = False
+    """When enabled, additional output is available"""
+
+    def configure(self):
+        self.add_argument('pattern')
+        self.add_argument('target_directory')
+        self.add_argument('target_file_name')
+        self.add_argument('-v', '--verbose')
+
+
+def _get_arguments() -> CreateDistributeArgumentParser:
+    parser = CreateDistributeArgumentParser()
+    parsed_args = parser.parse_args()
+    return parsed_args
+
+
+def distribute(directory: str, distribution: Distribution, config):
+    """Distributes all files in a directory.
+    """
+    distributed_files = set()
+    for candidate_file in listdir(directory):
+        distribution_match = distribution.match(candidate_file)
+        if distribution_match is not None:
+            info("Checking %s", candidate_file)
+
+            target_directory = distribution_match.target_directory()
+            debug(target_directory)
+
+            target_file = distribution_match.target_file()
+            debug(target_file)
+
+            Path(target_directory).mkdir(parents=True, exist_ok=True)
+            source = path.join(directory, candidate_file)
+            destination = path.join(target_directory, target_file)
+            if destination in distributed_files:
+                raise ValueError(
+                    "{} would be overwritten by {}".format(target_file, candidate_file))
+            _transfer(source, destination, config)
+            distributed_files.add(destination)
+
+
+def _transfer(source: str, destination: str, config) -> None:
+    log_text = "%s from: '%s' to '%s'"
+    if config.check:
+        info(log_text, "Distribute", source, destination)
+    else:
+        if config.copy:
+            info(log_text, "Copy", source, destination)
+            copyfile(source, destination)
+        else:
+            info(log_text, "Move", source, destination)
+            move(source, destination)
+
+
+# Execute the main script
+if __name__ == '__main__':
+    args = _get_arguments()
+    log_level = DEBUG if args.verbose else INFO
+    basicConfig(format='%(message)s', encoding='utf-8', level=log_level)
+    input_distribution = Distribution(args.pattern, args.target_directory, args.target_file_name)
+    distribute(args.directory, input_distribution, args)
---
+++ tests/test_distribute.py
@@ -0,0 +1,98 @@
+from collections import namedtuple
+from unittest import TestCase, main
+from tempfile import TemporaryDirectory
+from os import mkdir, path
+
+from distribute import distribute, Distribution
+
+MockArgSpace = namedtuple('MockArgSpace', ['copy', 'check'])
+
+_TEST_PATTERN = r"prefix-([a-z0-9]*)_(\d{4})-(\d{2})-(\d{2})([a-z-]*)"
+_TEST_DIRECTORY = "/target/directory/{1}"
+_TEST_FILE_NAME = "{1}-{2}-{3}_{0}"
+_MATCHING_INPUT = "prefix-move2end_2021-03-19"
+_EXPECTED_DIRECTORY = "/target/directory/2021"
+_EXPECTED_FILE = "2021-03-19_move2end"
+
+
+class TestDistribution(TestCase):
+
+    def test_distribution_initialization(self):
+        distribution = Distribution("", _TEST_DIRECTORY, _TEST_FILE_NAME)
+        self.assertEqual(distribution.target_directory, _TEST_DIRECTORY)
+        self.assertEqual(distribution.target_directory_indices, [1])
+        self.assertEqual(distribution.target_file_name, _TEST_FILE_NAME)
+        self.assertEqual(distribution.target_file_name_indices, [1, 2, 3, 0])
+
+    def test_distribution_non_match(self):
+        distribution = Distribution(_TEST_PATTERN, "", "")
+        result = distribution.match("non-matching")
+        self.assertEqual(result, None)
+
+    def test_distribution_match(self):
+        distribution = Distribution(_TEST_PATTERN, "", "")
+        result = distribution.match(_MATCHING_INPUT)
+        assert result is not None
+
+
+class TestDistributionMatch(TestCase):
+
+    def test_replacement(self):
+        fixture = Distribution(_TEST_PATTERN, _TEST_DIRECTORY, _TEST_FILE_NAME) \
+            .match(_MATCHING_INPUT)
+        self.assertEqual(fixture.target_directory(), _EXPECTED_DIRECTORY)
+        self.assertEqual(fixture.target_file(), _EXPECTED_FILE)
+
+
+class TestDistributionApplication(TestCase):
+
+    def setUp(self):
+        self.distribution_source = None
+        self.distribution_target = None
+
+    def _create_dirs(self, root_dir: str) -> None:
+        self.distribution_source = path.join(root_dir, "distribution_source")
+        mkdir(self.distribution_source)
+        self.distribution_target = path.join(root_dir, "distribution_target")
+        mkdir(self.distribution_target)
+
+    def _create_test_file(self, file):
+        with open(path.join(self.distribution_source, file), 'w'):
+            pass
+        assert path.exists(path.join(self.distribution_source, file)) is True
+
+    def test_distribution(self):
+        with TemporaryDirectory() as test_root:
+            self._create_dirs(test_root)
+            self._create_test_file(_MATCHING_INPUT)
+            self._create_test_file("non-matching")
+            test_distribution = Distribution(_TEST_PATTERN, self.distribution_target + "/{1}",
+                                             _TEST_FILE_NAME)
+            config = MockArgSpace(False, False)
+            distribute(self.distribution_source, test_distribution, config)
+
+            expected_distributed = path.join(self.distribution_target, "2021", _EXPECTED_FILE)
+            assert path.exists(expected_distributed) is True
+            expected_not_distributed = path.join(self.distribution_target, "non-matching")
+            assert path.exists(expected_not_distributed) is False
+
+    def test_failure(self):
+        with TemporaryDirectory() as test_root:
+            self._create_dirs(test_root)
+            self._create_test_file(_MATCHING_INPUT)
+            self._create_test_file(_MATCHING_INPUT + "-would-overwrite")
+
+            # The two files would be mapped to the same file
+            test_distribution = Distribution(_TEST_PATTERN, self.distribution_target + "/{1}",
+                                             _TEST_FILE_NAME)
+            config = MockArgSpace(False, False)
+            self.assertRaises(ValueError, distribute, self.distribution_source, test_distribution,
+                              config)
+
+            # The first file is distributed nonetheless
+            expected_distributed = path.join(self.distribution_target, "2021", _EXPECTED_FILE)
+            assert path.exists(expected_distributed) is True
+
+
+if __name__ == '__main__':
+    main()
